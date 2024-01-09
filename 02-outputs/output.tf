c5e77826a085e6a218cde0509f011acea00cf6a9
Afroz <afrozalikhan81@gmail.com>
2024-01-09 11:44:25 +0530

    terraform

---
+++ 02-outputs/output.tf
@@ -0,0 +1,11 @@
+output "sample" {
+  value = "Hello world"
+}
+
+# Any string in terraform need to be quoted with double quotes only
+# Single quotes are not supported
+
+
+output "sample1" {
+  value = 1000
+}
