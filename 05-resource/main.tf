3ac3b0f6c193e2afba23054d199a53a6610cbabd
Afroz <afrozalikhan81@gmail.com>
2024-01-11 15:58:15 +0530

    Resource in terraform

--- 05-resource/main.tf
+++ 05-resource/main.tf
@@ -1,14 +1,14 @@
-  resource "aws_instance" "web" {
+resource "aws_instance" "web" {
     ami           = data.aws_ami.example.id
     instance_type = "t3.micro"
     
     tags = {
-    Name = "HelloWorld"
+      Name = "HelloWorld"
     }
-  }
-  
-  data "aws_ami" "example" {
-  owners = ["973714476881"]
+}
+
+data "aws_ami" "example" {
+  owners           = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  }
+}
