a40fadb9869ae81e7a636f8cf6e0c368441d091e
Afroz <afrozalikhan81@gmail.com>
2024-01-12 18:28:15 +0530

    remote state

--- 10-loops/count/main.tf
+++ 10-loops/count/main.tf
@@ -13,3 +13,8 @@ data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 }
+
+variable "instances" {
+  default = ["frontend", "catalogue", "cart"]
+}
+
